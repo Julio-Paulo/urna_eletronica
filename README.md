334af017937faa42cc89ced0c725b65b6d360342
Julio-Paulo <julio-paulo@hotmail.com>
2023-01-02 23:54:57 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Projeto Urna Eletronica
+
+Projeto em JavaScript, fazendo uma urna eletronica.
+
+Numero dos Candidatos:
+
+Vereador: 38111, 77222 e 20000
+
+Prefeito: 99, 84
+
+Projeto desenvolvido durante o curso [B7web]<https://b7web.com.br>
---
+++ etapas.js
@@ -0,0 +1,59 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'All Might',
+                partido: 'UA',
+                fotos: [
+                    {url:'allmight.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Saitama',
+                partido: 'Associação de Heróis',
+                fotos: [
+                    {url:'saitama.jpg', legenda: 'Vereador' }
+                ]
+            },
+            {
+                numero: '20000',
+                nome: 'Rengoku Kyōjurō',
+                partido: 'Hashira',
+                fotos: [
+                    {url:'rengoku.jpeg', legenda: 'Vereador' }
+                ]
+            }
+        ] 
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Monkey D. Luffy',
+                partido: 'Mugiwara',
+                vice: 'Roronoa Zoro',
+                fotos: [
+                    {url:'luffy.png', legenda: 'Prefeito'},
+                    {url:'zoro.jpeg', legenda: 'Vice-Prefeito', small:true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Uzumaki Naruto',
+                partido: 'Konoha',
+                vice: 'Uchiha Sasuke',
+                fotos: [
+                    {url:'naruto.jpg', legenda: 'Prefeito'},
+                    {url:'sasuke.jpg', legenda: 'Vice-Prefeito', small:true}
+                ]
+            }
+        ]
+    }
+]
+    
